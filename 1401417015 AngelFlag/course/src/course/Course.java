b10662a860123401f90e2eb13ea32bef9608da82
THE NET <THE NET@10.1.5.38>
2014-11-30 11:15:41 +0200

    that is task 11, exam 1.

---
+++ 1401417015 AngelFlag/course/src/course/Course.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package course;
+
+/**
+ *
+ * @author THE NET
+ */
+import java.util.Scanner;
+
+public class Course {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner Keyboard= new Scanner(System.in);
+        boolean exit = false;
+        
+        do {
+            System.out.println("type 'exit' to exit this program...");
+            input = Keyboard.nextLine();
+            if(input.equals("exit")){
+                exit = true;
+        }while(exit == false);
+       
+                
+            }
+        }while(exit == false);
+        System.out.println("Goodbye");
+        
+            }
+        }
+    
+        Boolean hello = true;
+        
+        while(hello)
+        {
+            System.out.println("Hello");
+            hello = false;
+            
+        }
+    }
+        
+        
