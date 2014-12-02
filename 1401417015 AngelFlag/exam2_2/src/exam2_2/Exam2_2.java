0860d836052c147511cbc13629a3110bccd93861
THE NET <THE NET@FlagBg>
2014-12-02 19:32:32 +0200

    that is task 2, exam 2. field in Eclipse. grrrrrr counting 20 19 18 17

--- 1401417015 AngelFlag/exam2_2/src/exam2_2/Exam2_2.java
+++ 1401417015 AngelFlag/exam2_2/src/exam2_2/Exam2_2.java
@@ -10,12 +10,15 @@ package exam2_2;
  * @author THE NET
  */
 public class Exam2_2 {
-
+//otpechataite chislata ot 1 do 20 v obraten red
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+            for(int i=20; i>1; i=i-1)
+
+ System.out.println(i);
     }
     
 }
